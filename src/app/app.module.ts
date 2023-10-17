e2689e69f4829f3ca0113e07aeb4375fec94b8fa
dearestdust071@gmail.com <dearestdust071@gmail.com>
2023-10-17 17:01:59 -0600

    Agregado el private -J

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { PrivateComponent } from './private/private.component';
 @NgModule({
   declarations: [
     AppComponent,
+    PrivateComponent,
   ],
   imports: [
     BrowserModule,
